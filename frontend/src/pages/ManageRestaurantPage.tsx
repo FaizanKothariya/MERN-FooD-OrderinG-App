3c7152a38dc301e1802d01d42f5f287522de3f0d
Faizan Kothariya <kothariyafaizan2001@gecg28.ac.in>
2024-05-03 19:45:39 +0530

    Create Restaurant Form

---
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,12 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/form/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
+};
+
+export default ManageRestaurantPage;
